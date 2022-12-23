67f4e507ed3b4e681bddce12c2ea2010ca3c3c59
Noah Holt <noahryanholt@gmail.com>
2022-12-23 00:47:58 -0600

    added date added to vinyl

--- parseVinylCollection.js
+++ parseVinylCollection.js
@@ -4,9 +4,11 @@ const parseVinylCollection = (vinylCollection) => {
     }
 
     return vinylCollection.releases.map((release) => {
+        const { date_added: dateAdded } = release;
         const { title, thumb, artists } = release.basic_information;
         let artistsArr = artists.map((artist) => artist.name);
         return {
+            dateAdded,
             album: title,
             artists: artistsArr,
             thumbnail: thumb,
